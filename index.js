633fd8af0a08ea9f98734e8c6055d2018c88ca42
k1no1502 <127868561+k1no1502@users.noreply.github.com>
2025-07-16 22:01:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js');
+const keep_alive = require('./keep_alive.js')
+
+
 
 var username = process.env.username;
 var password = process.env.password;
@@ -9,20 +11,13 @@ var shared_secret = process.env.shared;
 var games = [730, 1238840, 1238810, 1517290, 1238860, 1238820, 1238880, 307780, 976310, 1110910, 1293830, 230410, 2669320];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
-// ➤ Đây là đoạn đăng nhập chính, đã thêm dòng tạo mã 2FA từ shared_secret
-const user = new steamUser();
-const logOnOptions = {
-  accountName: username,
-  password: password,
-  twoFactorCode: steamTotp.generateAuthCode(shared_secret)  // ✅ dòng này tự tạo mã 2FA
-};
 
-user.logOn(logOnOptions);
-
+user = new steamUser();
+user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
-  if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-  user.setPersona(status);               
-  user.gamesPlayed(games);
+	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
+	user.setPersona(status);               
+	user.gamesPlayed(games);
 });
 
 // Bạn có thể bật lại tài khoản thứ 2 nếu muốn:
